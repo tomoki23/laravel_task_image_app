fa1b40422020d4c60ed16cd825eb48342d772eb6
tomoki <tmdesign.0223@gmail.com>
2023-06-04 09:03:47 +0900

    storeメソッド作成

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\Category;
 use App\Models\Task;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -24,5 +25,31 @@ class TaskController extends Controller
         $categories = Category::all();
 
         return view('tasks.create', compact('users', 'categories'));
+    }
+
+    public function store(Request $request)
+    {
+        $userId = $request->input('user_id');
+        $categoryId = $request->input('category_id');
+        $title = $request->input('title');
+        $body = $request->input('body');
+        $imagePath = '';
+        if ($request->file('image')) {
+            $imagePath = $request->file('image')->store('image', 'public');
+        }
+        $firstStatusCode = 3;
+
+        Task::create([
+            'user_id' => $userId,
+            'category_id' => $categoryId,
+            'title' => $title,
+            'image_path' => $imagePath ? $imagePath : '',
+            'body' => $body,
+            'status' => $firstStatusCode,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
+        return to_route('tasks.index');
     }
 }
