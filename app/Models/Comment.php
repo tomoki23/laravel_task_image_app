2e932e902e34f464a934ad0223e54649af1f80cf
tomoki <tmdesign.0223@gmail.com>
2023-06-25 23:08:36 +0900

    fillable追加

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,6 +9,12 @@ class Comment extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'user_id',
+        'task_id',
+        'body'
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
