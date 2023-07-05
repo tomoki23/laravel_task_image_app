d1c8cc71a5996008d0bbcd53d77f783c8fcd612b
tomoki <tmdesign.0223@gmail.com>
2023-07-05 23:43:15 +0900

    CommentControlerにdestroyメソッド作成

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,12 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateCommentRequest;
 use App\Models\Comment;
+use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
     public function store(CreateCommentRequest $request)
     {
-        dd($request);
         Comment::create([
             'user_id' => $request->user()->id,
             'task_id' => $request->id,
@@ -17,5 +17,13 @@ class CommentController extends Controller
         ]);
 
         return to_route('tasks.show', ['id' => $request->id]);
+    }
+
+    public function destroy($id, Request $request)
+    {
+        $taskId = $request->input('task_id');
+        Comment::destroy($id);
+
+        return to_route('tasks.show', ['id' => $taskId]);
     }
 }
