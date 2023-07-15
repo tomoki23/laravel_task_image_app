1a4fd2a35863bc2de042506e85f1ac80f0b58b79
tomoki <tmdesign.0223@gmail.com>
2023-07-15 16:10:10 +0900

    user_idにcascadeOnDeleteを追加

--- database/migrations/2023_05_31_143830_create_comments_table.php
+++ database/migrations/2023_05_31_143830_create_comments_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
             $table->foreignId('task_id')
                 ->constrained()
                 ->cascadeOnDelete();
